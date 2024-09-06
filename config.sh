3ade9515906c80dc3c8b19ec801330768f643bfe
SrCrow02 <guibio24+oldgithub@proton.me>
2024-09-06 17:26:55 -0300

    fixing

--- config.sh
+++ config.sh
@@ -5,16 +5,16 @@ sudo apt update && sudo apt upgrade -y
 # Instalar pacotes essenciais
 sudo apt install -y vim neovim falkon git curl build-essential kitty
 
-# Criar um novo usuário e definir senha
-sudo useradd -m new_user
-echo "new_user:password" | sudo chpasswd
-sudo usermod -aG sudo new_user
+# Create a new user and password
+#sudo useradd -m new_user
+#echo "new_user:password" | sudo chpasswd
+#sudo usermod -aG sudo new_user
 
-# Instalar Node.js (versão 14) e Docker
+# Download Docker and Nodejs 
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs docker.io neofetch
 
-# Baixar e instalar o Rust
+# Download Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
@@ -27,9 +27,19 @@ echo "deb https://deb.bitwarden.com/ stable main" | sudo tee /etc/apt/sources.li
 sudo apt update
 sudo apt install -y bitwarden
 
+sudo apt install -y wget gnupg
+
+wget -qO - https://deb.torproject.org/torproject.org/archive-key.asc | sudo apt-key add -
+echo "deb https://deb.torproject.org/torproject.org focal main" | sudo tee /etc/apt/sources.list.d/tor.list
+
+# Download TOR Browser
+sudo apt update
+sudo apt install -y tor tor-geoipdb
+
+# Git configuration
 git config --global user.name "Seu nome"
 git config --global user.email "seu_email@example.com"
-git config --global core.editor "vim"
+git config --global core.editor "neovim"
 
 git config --global alias.co checkout
 git config --global alias.br branch
@@ -38,4 +48,3 @@ git config --global alias.st status
 
 echo '*.log' >> ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
-
